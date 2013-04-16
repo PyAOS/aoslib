5baaaa8d6960bf88d0930119f834ddd625825182
Jonathan Helmus <jjhelmus@gmail.com>
2013-04-16 13:14:46 -0500

    created awips module for AWIPS I functions

---
+++ aoslib/__init__.py
@@ -0,0 +1,12 @@
+"""
+======
+aoslib
+======
+
+aoslib is a Python library of standard atmospheric and oceanic sciences
+calculation routines.  It exists mainly so we're all not writing our
+own routines to calculate potential temperature, isentropic potential
+vorticity, etc.
+"""
+
+from awips import *
